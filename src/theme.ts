3c47eab16cd99bade7bf33158cb12cd5a5e7a16b
kristersgcode <kristersgutmanis@gmail.com>
2026-09-11 17:58:47 +0300

    chore: update theme colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 export const colors = {
   white: "#FFFFFF",
-  purple: "#663399",
-  yellow: "#FFDE59",
+  purple: "#472A6E",
+  yellow: "#F5E74D",
   black: "#000000",
   lightGray: "#E5E5E5",
 } as const;
